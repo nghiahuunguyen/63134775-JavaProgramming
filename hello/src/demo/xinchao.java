6a7734ba4ad54227ea8203d69d5db17044c98ce0
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2024-01-19 14:05:57 +0700

    vidu

---
+++ hello/src/demo/xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào");
+	}
+
+}
